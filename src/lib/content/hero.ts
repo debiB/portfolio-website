1d4c271c92f1aaaee98fad759b332e44b52d0651
deborah <dibora.berhanu@a2sv.org>
2024-08-19 17:18:16 +0300

    refined the hero section

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -4,7 +4,7 @@ import { resumeFileName } from '@/lib/utils/config';
 export const heroSection: HeroSectionType = {
   subtitle: 'Hi, my name is',
   title: 'Dibora Berhanu.',
-  tagline: 'I build full-stack web apps and stunning interfaces.',
+  tagline: 'I build full-stack web apps.',
   description:
     "I'm a passionate full-stack web developer with hands-on experience in building web applications using React.js, Next.js with TypeScript, TailwindCSS, Node.js, and ASP.NET, along with UI/UX design skills.",
   specialText: '',
