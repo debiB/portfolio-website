e8cb517b35397726ac3223058441b90841b88bbd
deborah <dibora.berhanu@a2sv.org>
2024-08-19 16:50:20 +0300

    removed facebook and instagram icon

--- src/lib/content/social.ts
+++ src/lib/content/social.ts
@@ -8,16 +8,8 @@ export const socialSection: SocialSectionType = {
       url: socialLinks.github,
     },
     {
-      icon: 'mdi:instagram',
-      url: socialLinks.instagram,
-    },
-    {
       icon: 'lucide:linkedin',
       url: socialLinks.linkedin,
-    },
-    {
-      icon: 'lucide:facebook',
-      url: socialLinks.facebook,
     },
   ],
 };
