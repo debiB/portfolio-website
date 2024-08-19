2c277c7b069568a0b048386b02f3225156a4d2d6
deborah <dibora.berhanu@a2sv.org>
2024-08-19 16:17:01 +0300

    chenged tab name

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -19,12 +19,12 @@ export const author = {
 };
 
 export const seoData = {
-  title: 'Vatsal Singh | Front-End Developer',
+  title: 'Dibora Berhanu Portfolio',
   description:
-    'Vatsal Singh is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+    '',
   author: author.name,
   image:
-    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+    '',
   url: 'https://vatsalsinghkv.vercel.app/',
   keywords: [
     'Dibora',
